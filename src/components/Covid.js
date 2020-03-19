f6b427433518cfdcf487a4d327daecb799e07a97
Mustofa <mustofa_sachi@hotmail.com>
2020-03-20 02:28:19 +0700

    get covid from new api and set proxy

--- src/components/Covid.js
+++ src/components/Covid.js
@@ -2,22 +2,16 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const Covid = () => {
-    const [covidStats, setCovidStats] = useState({})
-
     const getCovidStats = async () => {
-        // let config = {
-        //     headers: {
-        //         "Cache-Control": "no-cache",
-        //         "Subscription-Key": "0e142d3ec4bb4a78af9dbe7285b44170"
-        //     }
-        // }
+        let config = {
+            headers: {
+                "Cache-Control": "no-cache",
+                "Subscription-Key": "0e142d3ec4bb4a78af9dbe7285b44170"
+            }
+        }
 
-        // const result = await axios.get(`https://api.smartable.ai/coronavirus/stats/global`, config)
-        // console.log(result)
-
-        const result = await axios.get(`https://corona.lmao.ninja/all`)
+        const result = await axios.get(`/coronavirus/stats/global`, config)
         console.log(result.data)
-        setCovidStats(result.data)
     }
 
     useEffect(() => {
@@ -27,9 +21,6 @@ const Covid = () => {
     return (
         <div>
             <h4>Get Covid Stats</h4>
-            <p>Case: {covidStats.cases}</p>
-            <p>Deaths: {covidStats.deaths}</p>
-            <p>Recovered: {covidStats.recovered}</p>
         </div>
     )
 }
