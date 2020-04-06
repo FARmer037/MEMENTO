5d7c631fc299cb77847ac30283d4b6260ecd6603
Mustofa <mustofa_sachi@hotmail.com>
2020-04-06 11:56:35 +0700

    เปลี่ยนรูปแบบ Dashboard, ดึงข้อมูล Firebase

--- src/App.js
+++ src/App.js
@@ -3,20 +3,16 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Covid from './components/covid/Covid';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
-import StoryDetails from './components/stoties/StoryDetails';
 import CreateStory from './components/stoties/CreateStory';
-// import Navbar2 from './components/layout/Navbar2';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <Navbar />
-        {/* <Navbar2 /> */}
         <Switch>
           <Route exact path='/' component={Dashboard} />
           <Route path='/covid' component={Covid} />
-          <Route path='/story/:id' component={StoryDetails} />
           <Route path='/create' component={CreateStory} />
         </Switch>
       </div>
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,20 +1,44 @@
-import React from 'react'
-import Notification from './Notification'
-import StoryList from '../stoties/StoryList'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { Timeline, Typography } from 'antd'
+import { ClockCircleOutlined } from '@ant-design/icons'
+import { firestore } from '../../index'
 
-const Dashboard = (props) => {
-    const { story } = props
+const { Title } = Typography;
+
+const Dashboard = () => {
+    const stories = useSelector(state => state.story)
+    const dispatch = useDispatch()
+
+    const retriveData = () => {
+        firestore.collection("stories").onSnapshot((snapshot) => {
+            let newStory = snapshot.docs.map(d => {
+                // console.log(d.data())
+                const {id, task, location, date, startTime, endTime} = d.data()
+                // console.log(id, task, location, date, startTime, endTime)
+                return {id, task, location, date, startTime, endTime}
+            })
+            // console.log('new', newStory)
+            dispatch({ type: 'GET_STORY', stories: newStory })
+        })
+    }
+
+    useEffect(() => {
+        retriveData()
+    }, [])
+
     return (
-        <div className='dashboard container'>
-            <div className='row'>
-                <div className='col s12 m6'>
-                    <StoryList stories={story} />
-                </div>
-                <div className='col s12 m5 offset-m1'>
-                    <Notification />
-                </div>
-            </div>
-        </div>
+        <Timeline mode='left' style={{ margin: '20px 0px' }}>
+            {
+                stories.map(story => (
+                    <Timeline.Item dot={<ClockCircleOutlined onClick={() => console.log('clicked')} style={{ fontSize: '16px' }} />} label={story.date}>
+                        <Title level={4}>{story.task}</Title>
+                        <p>{story.location}</p>
+                        <p>{story.startTime} to {story.endTime}</p>
+                    </Timeline.Item>
+                ))
+            }
+        </Timeline>
     )
 }
 
