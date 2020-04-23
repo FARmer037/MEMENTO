9a4722324b52282296ac4ea37dbb29d582fea0db
Mustofa <mustofa_sachi@hotmail.com>
2020-04-23 18:21:09 +0700

    เพิ่ม delete, eddit ข้อมูล

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,26 +1,34 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { Timeline, Typography, Empty, Button, Row, Col } from 'antd'
+import { Timeline, Typography, Empty, Button, Row, Col, Drawer, Form, Input, DatePicker, TimePicker, Popover, Modal } from 'antd'
 import { ClockCircleOutlined, DownloadOutlined } from '@ant-design/icons'
 import { firestore } from '../../index'
 import jsPDF from 'jspdf'
 import 'jspdf-autotable'
+import moment from 'moment'
 
 const { Title } = Typography;
 
+const layout = {
+    labelCol: { span: 4 },
+    wrapperCol: { span: 17 },
+}
+
 const Dashboard = () => {
     const stories = useSelector(state => state.story)
+    const form = useSelector(state => state.form)
     const dispatch = useDispatch()
+
+    const [visible, setVisible] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [id, setId] = useState(0)
 
     const retriveData = () => {
         firestore.collection("stories").onSnapshot((snapshot) => {
             let newStory = snapshot.docs.map(d => {
-                // console.log(d.data())
                 const { id, task, location, date, startTime, endTime } = d.data()
-                // console.log(id, task, location, date, startTime, endTime)
                 return { id, task, location, date, startTime, endTime }
             })
-            // console.log('new', newStory)
             dispatch({ type: 'GET_STORY', stories: newStory })
         })
     }
@@ -42,6 +50,63 @@ const Dashboard = () => {
         doc.save('Timeline.pdf')
     }
 
+    const onFinish = () => {
+        firestore.collection("stories").doc(id + '').set({
+            id,
+            task: form.task,
+            location: form.location,
+            date: form.date,
+            startTime: form.startTime,
+            endTime: form.endTime
+        })
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+            setVisible(false)
+        }, 2000)
+    };
+
+    const onFinishFailed = errorInfo => {
+        console.log('Failed:', errorInfo);
+    }
+
+    const changeDate = (date, dateString) => {
+        dispatch({ type: 'CHANGE_DATE', date: dateString })
+    }
+    const changeStartTime = (date, timeString) => {
+        dispatch({ type: 'CHANGE_START_TIME', startTime: timeString })
+    }
+    const changeEndtTime = (date, timeString) => {
+        dispatch({ type: 'CHANGE_END_TIME', endTime: timeString })
+    }
+
+    const setid = id => {
+        setId(id)
+        showModal()
+    }
+
+    const deleteTask = () => {
+        firestore.collection("stories").doc(id + '').delete()
+        setVisible(false)
+    }
+
+    const showModal = () => {
+        setVisible(true)
+    }
+
+    const handleOk = () => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+            setVisible(false)
+        }, 3000)
+    }
+
+    const handleCancel = () => {
+        console.log(id)
+        setVisible(false)
+    }
+
     const showData = () => {
         if (stories && stories.length) {
             return (
@@ -50,8 +115,8 @@ const Dashboard = () => {
                         <Timeline mode='left' style={{ margin: '20px 0px' }}>
                             {
                                 stories.map((story, index) => (
-                                    <Timeline.Item key={index} dot={<ClockCircleOutlined onClick={() => console.log(index + 1)} style={{ fontSize: '16px' }} />} label={story.date}>
-                                        <Title level={4}>{story.task}</Title>
+                                    <Timeline.Item key={index} dot={<ClockCircleOutlined style={{ fontSize: '16px' }} />} label={story.date}>
+                                        <Title style={{ cursor: 'pointer' }} level={4} onClick={() => setid(story.id)}>{story.task}</Title>
                                         <p>{story.location}</p>
                                         <p>{story.startTime} to {story.endTime}</p>
                                     </Timeline.Item>
@@ -64,6 +129,67 @@ const Dashboard = () => {
                             Generate PDF
                         </Button>
                     </Col>
+                    <Modal
+                        visible={visible}
+                        title="Manage Story"
+                        onOk={handleOk}
+                        onCancel={handleCancel}
+                        footer={[
+                            <Button key="back" type="primary" danger onClick={deleteTask}>
+                                Delete
+                            </Button>,
+                            <Button key="submit" type="primary" loading={loading} onClick={onFinish}>
+                                Edit
+                            </Button>,
+                        ]}
+                    >
+                        <Form
+                            {...layout}
+                            name="basic"
+                            onFinish={onFinish}
+                            onFinishFailed={onFinishFailed}
+                        >
+                            <Form.Item
+                                label="Task"
+                                name="task"
+                                rules={[{ required: true, message: 'Please input a task!' }]}
+                            >
+                                <Input onChange={e => dispatch({ type: 'CHANGE_TASK', task: e.target.value })} />
+                            </Form.Item>
+
+                            <Form.Item
+                                label="Location"
+                                name="location"
+                                rules={[{ required: true, message: 'Please input a task!' }]}
+                            >
+                                <Input onChange={e => dispatch({ type: 'CHANGE_LOCATION', location: e.target.value })} />
+                            </Form.Item>
+
+                            <Form.Item
+                                label="Date"
+                                name="date"
+                                rules={[{ required: true, message: 'Please input date!' }]}
+                            >
+                                <DatePicker style={{ width: '100%' }} onChange={changeDate} />
+                            </Form.Item>
+
+                            <Form.Item
+                                label="Start Time"
+                                name="start-time"
+                                rules={[{ required: true, message: 'Please input date!' }]}
+                            >
+                                <TimePicker style={{ width: '100%' }} onChange={changeStartTime} defaultOpenValue={moment('00:00:00', 'HH:mm:ss')} />
+                            </Form.Item>
+
+                            <Form.Item
+                                label="End Time"
+                                name="end-time"
+                                rules={[{ required: true, message: 'Please input date!' }]}
+                            >
+                                <TimePicker style={{ width: '100%' }} onChange={changeEndtTime} defaultOpenValue={moment('00:00:00', 'HH:mm:ss')} />
+                            </Form.Item>
+                        </Form>
+                    </Modal>
                 </Row>
             )
         } else {
